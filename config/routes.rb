fd42ec08a8d58bec90430b48a7adba8e00ee2fa5
Thomé Lucas <chacalgbi@hotmail.com>
2021-03-21 09:28:09 -0300

    Controllers de acesso, I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :profile_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :profiles
+  devise_for :admins
   get 'welcome/index'
   get 'inicio', to: 'welcome#index'
 
