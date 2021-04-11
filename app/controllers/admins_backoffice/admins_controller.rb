2282c9ff2d3642761eb95eb90747ecc332eb07dd
Thomé Lucas <chacalgbi@hotmail.com>
2021-04-11 18:47:04 -0300

    Alter Controllers

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,20 +1,38 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verifica_senha, only: [:update]
+  before_action :set_admin, only: [:edit, :update]
+
   def index
   	@admins = Admin.all
   end
 
   def edit
-  	@admin = Admin.find(params[:id])
   end
 
   def update
-  	admin = Admin.find(params[:id])
-	params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-	if admin.update(params_admin)
+	if @admin.update(ajustar_parametros)
 		redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
 	else
 		render :edit
 	end
+  end
+
+
+
+  private
+
+  def ajustar_parametros
+  	params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_admin
+  	@admin = Admin.find(params[:id])
+  end
+
+  def verifica_senha
+  	if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+  	   params[:admin].extract!(:password, :password_confirmation)
+  	end
   end
 
 end
