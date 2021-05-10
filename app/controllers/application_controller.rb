502c72b4257759765a48403bcb2791fd65dc1119
Thomé Lucas <chacalgbi@hotmail.com>
2021-05-10 15:27:30 -0300

    Adjusting User, Index and Administrative Layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,26 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def layout_by_resource
+		devise_controller? ? "#{resource_class.to_s.downcase}_devise" : "application"
+	end
+
+end
+
+
+
+
+=begin
+
+	Mesma coisa do código acima
+	
+	def layout_by_resource
 		if devise_controller? && resource_class == Admin
 			"admin_devise"
+		elsif devise_controller? && resource_class == User
+			"user_devise"
 		else
 			"application"
 		end
 	end
-
-end
+	
+=end
