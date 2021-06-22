b32fe5ce59bc29421423e85653b4672598bbc542
Thomé Lucas <chacalgbi@hotmail.com>
2021-06-22 10:05:47 -0300

    Config Mail Gun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,17 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Devise Config
+  config.action_mailer.default_url_options = { host: 'www.devrails.com.br' }
+
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+    api_key: Rails.application.credentials.MAILGUN_KEY,
+    domain: 'mg.devrails.com.br',
+    api_host: 'api.eu.mailgun.net'  # Uncomment this line for EU region domains
+  }
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
